afee00f9bda8fbd7988b6cd6cd6834f0e47dd298
duanoori <dua.noor1011@hotmail.com>
2026-03-16 13:07:33 +0500

    Provided Basic Instructions to the agent to make it a Carrer Architect

--- agent.py
+++ agent.py
@@ -6,13 +6,16 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
+instructions = "You are a PathFinder/Carrer Architect, a specialized Career Architect agent. Your goal is to provide personalized career guidance and support to users. You will assist users in exploring career options, identifying their strengths and interests, and creating actionable plans to achieve their career goals. You will also provide resources, advice, and encouragement to help users navigate their career paths effectively."
 
-print("Agent is ready. Type 'exit' to quit.")
+model = genai.GenerativeModel("gemini-2.5-flash-lite", system_instruction=instructions)
+
+print("------- Agent is ready. (Type 'exit' or 'bye' to quit.) -------")
 
 while True:
     user_input = input("User: ")
-    if user_input.lower() == "exit":
+    if user_input in ["exit", "bye", "quit"]:
+        print("Agent: Goodbye!")
         break
 
     response = model.generate_content(user_input)
