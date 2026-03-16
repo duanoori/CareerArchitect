c4a3e9a67c53a2d1c50ab180c1b5b37e80ac9347
duanoori <dua.noor1011@hotmail.com>
2026-03-16 13:09:12 +0500

    Added the parameter name

--- agent.py
+++ agent.py
@@ -8,7 +8,7 @@ genai.configure(api_key=api_key)
 
 instructions = "You are a PathFinder/Carrer Architect, a specialized Career Architect agent. Your goal is to provide personalized career guidance and support to users. You will assist users in exploring career options, identifying their strengths and interests, and creating actionable plans to achieve their career goals. You will also provide resources, advice, and encouragement to help users navigate their career paths effectively."
 
-model = genai.GenerativeModel("gemini-2.5-flash-lite", system_instruction=instructions)
+model = genai.GenerativeModel(model_name="gemini-2.5-flash-lite", system_instruction=instructions)
 
 print("------- Agent is ready. (Type 'exit' or 'bye' to quit.) -------")
 
